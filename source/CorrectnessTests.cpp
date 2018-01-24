be4e75cbabec41e6cf56e2d0691de86946743c56
Siddharth Atre <sidatre@gmail.com>
2018-01-24 10:30:43 -0800

    Confifs and edits

--- source/CorrectnessTests.cpp
+++ source/CorrectnessTests.cpp
@@ -2,12 +2,19 @@
 #include <glm/glm.hpp>
 #include "Matrix.hpp"
 
+using namespace std;
 int main() {
-	Matrix<float> m(4, 4);
+	Matrix<float> m1(4, 10, 113.123123123f);
+    Matrix<float> m2(10, 4, 12.f);
+    Matrix<float> m3(11, 4, 10.f);
+    
+    auto res1 = m1 * m2;
+    try {
+        auto res2 = m1 * m3;
+    } catch (invalid_argument & e) {
+        cout << "Caught exception" << endl;
+    }
 
-	for (int i = 0; i < 4; ++i)
-		for (int j = 0; j < 4; ++j)
-			std::cout << m.Get(i, j) << std::endl;
     
 	return 0;
 }
--- source/Matrix.hpp
+++ source/Matrix.hpp
@@ -6,21 +6,25 @@ template <class T>
 class Matrix
 {
 public:
-	Matrix(size_t w, size_t h);
+    Matrix(size_t numRows, size_t numCols, T defaultValue = 0);
 	~Matrix();
 	
 	/// Fetches the element at the given coordinates
-	const T & Get(size_t x, size_t y) const;
+	const T & Get(size_t row, size_t col) const;
 	/// Fetches the element at the given coordinates
-	T & Get(size_t x, size_t y);
+	T & Get(size_t row, size_t col);
+    size_t Rows() { return m_columns; }
+    size_t Columns() { return m_rows; }
+    Matrix operator*(const Matrix & rhs);
+    
 private:
 	/// Converts the 2D element coord to a 1D index
 	size_t Index(const size_t & x, const size_t & y) const;
-
+    
+    /// The # of rows in this matrix.
+    const size_t m_rows;
 	/// The # of columns in this matrix.
-	const size_t m_width;
-	/// The # of rows in this matrix.
-	const size_t m_height;
+	const size_t m_columns;
 	/**
 	 * The array holding the elements.
 	 * 1D array is more efficient than a 2D array with 
@@ -31,9 +35,9 @@ private:
 };
 
 template <class T>
-Matrix<T>::Matrix(size_t w, size_t h): m_width(w), m_height(h) {
-	m_data = new T[w * h];
-	std::fill(m_data, m_data + (w * h), 0);
+Matrix<T>::Matrix(size_t numRows, size_t numCols, T defaultValue): m_rows(numRows), m_columns(numCols) {
+	m_data = new T[numRows * numCols];
+	std::fill(m_data, m_data + (numRows * numCols), defaultValue);
 }
 
 template <class T>
@@ -42,18 +46,36 @@ Matrix<T>::~Matrix() {
 }
 
 template <class T>
-size_t Matrix<T>::Index(const size_t & x, const size_t & y) const {
-	return x + m_width * y;
+size_t Matrix<T>::Index(const size_t & row, const size_t & col) const {
+    return col * m_rows + row; // Matrix is stored in COLUMN major
 }
 
 template <class T>
-const T & Matrix<T>::Get(size_t x, size_t y) const {
-	if(x >= m_width || y >= m_height) throw std::invalid_argument( "Invalid element coordiante" );
-	return m_data[Index(x, y)];
+const T & Matrix<T>::Get(size_t row, size_t col) const {
+	if(row >= m_rows || col >= m_columns) throw std::invalid_argument( "Invalid element coordiante" );
+	return m_data[Index(row, col)];
 }
 
 template <class T>
-T & Matrix<T>::Get(size_t x, size_t y) {
-	if(x >= m_width || y >= m_height) throw std::invalid_argument( "Invalid element coordiante" );
-	return m_data[Index(x, y)];
+T & Matrix<T>::Get(size_t row, size_t col) {
+    if(row >= m_rows || col >= m_columns) throw std::invalid_argument( "Invalid element coordiante" );
+    return m_data[Index(row, col)];
+}
+
+template <class T>
+Matrix<T> Matrix<T>::operator*(const Matrix<T> & rhs) {
+    //My # of columns (width) must equal # of rows (height) in other matrix.
+    if(m_columns != rhs.m_rows)
+        throw std::invalid_argument("Invalid argument. Width of first matrix must match height of second matrix");
+    
+    //First pass attempt.
+    //TODO: Optimize across threads (OpenMP) & implement vector processing.
+    
+    //Note that the length of each row is num columns and vice versa.
+    
+    //Iterate over the elements of the result matrix.
+    Matrix<T> result(m_rows, rhs.m_columns);
+    
+    
+    return result;
 }
